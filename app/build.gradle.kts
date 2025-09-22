ced8a51cd80eeb3bca6d7a2e38871729d1fececf
rosanasato <rosana.sato@protonmail.com>
2025-09-22 13:23:27 -0300

    Add theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.9.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
